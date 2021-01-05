4212b681ef4c44b7f0e8f901b4d3c157fcac90af
Alexandria Robertson <67127042+alexemrob@users.noreply.github.com>
2021-01-05 16:52:24 -0600

    run build

---
+++ build/precache-manifest.7071b2dff7ceb1f142e70c02f269db83.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff8d1b1aa8ff8ea33c37debd57a7f017",
+    "url": "/OfficialPortfolio/index.html"
+  },
+  {
+    "revision": "6de9afbb91e59a9dadc8",
+    "url": "/OfficialPortfolio/static/css/2.7a917665.chunk.css"
+  },
+  {
+    "revision": "90885606f527996b2697",
+    "url": "/OfficialPortfolio/static/css/main.300acb78.chunk.css"
+  },
+  {
+    "revision": "6de9afbb91e59a9dadc8",
+    "url": "/OfficialPortfolio/static/js/2.b89b6d95.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/OfficialPortfolio/static/js/2.b89b6d95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90885606f527996b2697",
+    "url": "/OfficialPortfolio/static/js/main.24d6e270.chunk.js"
+  },
+  {
+    "revision": "389a41f9d589334e130b",
+    "url": "/OfficialPortfolio/static/js/runtime-main.47e78525.js"
+  }
+]);
